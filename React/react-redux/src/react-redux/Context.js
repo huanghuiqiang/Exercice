7b8a5bf0f17e7c4e74d3302fe3806f89ca5186f5
Hugh 2098 <huanghuiqiang86@gmail.com>
2022-03-24 15:55:23 +0800

    feat: add Provider function

---
+++ React/react-redux/src/react-redux/Context.js
@@ -0,0 +1,5 @@
+
+
+const ReactReduxContext = React.createContext();
+
+export default ReactReduxContext;
--- React/react-redux/src/react-redux/index.js
+++ React/react-redux/src/react-redux/index.js
@@ -1,1 +1,17 @@
 
+import ReactReduxContext from './Context';
+
+function Provider(props) {
+  const { store, children } = props;
+
+  const contextValue = { store };
+
+  return (
+    <ReactReduxContext.Provider value={contextValue}>
+      { children }
+    </ReactReduxContext.Provider>
+  )
+}
+
+
+export { Provider }
