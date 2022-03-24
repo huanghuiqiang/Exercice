d45ea5ad9490cf1117e37672b56f7672bbff0a19
Hugh 2098 <huanghuiqiang86@gmail.com>
2022-03-24 15:33:43 +0800

    feat: add react-redux

--- React/react-redux/src/App.js
+++ React/react-redux/src/App.js
@@ -1,11 +1,41 @@
 
+import { connect } from 'react-redux';
+import { increment, decrement, reset } from './action';
 
-function App() {
+function App(props) {
+  const { 
+    count,
+    incrementHandler,
+    decrementHandler,
+    resetHandler
+   } = props;
+
+
   return (
     <div>
-      hello,world
+      <h3>Count: {count}</h3>
+      <button onClick={incrementHandler}>计数+1</button>
+      <button onClick={decrementHandler}>计数-1</button>
+      <button onClick={resetHandler}>重置</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state.count
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    incrementHandler: () => dispatch(increment()),
+    decrementHandler: () => dispatch(decrement()),
+    resetHandler: () => dispatch(reset()),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
