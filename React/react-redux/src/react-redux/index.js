7764d2610b4c391496082d13090fdf620b02c914
Hugh 2098 <huanghuiqiang86@gmail.com>
2022-03-25 14:06:50 +0800

    feat: add subscription

--- React/react-redux/src/react-redux/index.js
+++ React/react-redux/src/react-redux/index.js
@@ -1,5 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useLayoutEffect, useReducer, useRef, useMemo, useEffect } from 'react';
 import ReactReduxContext from './Context';
+import { shallowEqual } from './util';
+import Subscription from './Subscription';
+
+function storeStateUpdatesReducer(count) {
+  return count + 1;
+}
 
 
 /**
@@ -10,7 +16,33 @@ import ReactReduxContext from './Context';
 function Provider(props) {
   const { store, children } = props;
 
-  const contextValue = { store };
+  // 这是要传递的context
+  // 里面放入store和subscription实例
+  const contextValue = useMemo(() => {
+    const subscription = new Subscription(store);
+
+    // 注册回调为通知子组件，这样就可以开始层级通知了
+    subscription.onStateChange = subscription.notifyNestedSubs;
+    return {
+      store,
+      subscription,
+    }
+  }, [store]);
+
+  // 拿到之前的state值
+  const previousState = useMemo(() => store.getState(), [store]);
+
+  // 每次contextValue或者previousState变化的时候
+  // 用notifyNestedSubs通知子组件
+  useEffect(() => {
+    const { subscription } = contextValue;
+    subscription.trySubscribe();
+
+    if (previousState !== store.getState()) {
+      subscription.notifyNestedSubs();
+    }
+
+  }, [contextValue, previousState])
 
   return (
     <ReactReduxContext.Provider value={contextValue}>
@@ -26,7 +58,21 @@ function Provider(props) {
  * @param {*} mapDispatchToProps 
  * @returns 
  */
-function connect(mapStateToProps, mapDispatchToProps) {
+function connect(
+  mapStateToProps = () => {},
+  mapDispatchToProps = () => {}
+  ) {
+
+  function childPropsSelector(store, wrapperProps) {
+    const state = store.getState();
+  
+      // 执行mapStateToProps和mapDispatchToProps
+      const stateProps = mapStateToProps(state);
+      const dispatchProps = mapDispatchToProps(store.dispatch);
+    
+      return Object.assign({}, stateProps, dispatchProps, wrapperProps);
+  }
+
   // 第一层函数接收mapStateToProps和mapDispatchToProps
   // 第二层函数是个高阶组件，里面获取context
   // 然后执行mapStateToProps和mapDispatchToProps
@@ -40,17 +86,56 @@ function connect(mapStateToProps, mapDispatchToProps) {
       const { ... wrapperProps } = props;
 
       // 获取context的值
-      const context = useContext(ReactReduxContext);
+      const contextValue = useContext(ReactReduxContext);
 
-      const { store } = context;
-      const state = store.getState();
+      const { store, subscription: parentSub } = contextValue;  // 解构出store和parentSub
 
-      // 执行mapStateToProps和mapDispatchToProps
-      const stateProps = mapStateToProps(state);
-      const dispatchProps = mapDispatchToProps(store.dispatch);
+      const actualChildProps = childPropsSelector(store, wrapperProps);
 
-      // 组装最终的props
-      const actualChildProps = Object.assign({}, stateProps, dispatchProps, wrapperProps);
+
+      // 记录上次渲染参数
+      const lastChildProps = useRef();
+      useLayoutEffect(() => {
+        lastChildProps.current = actualChildProps;
+      }, []);
+
+      // 使用useReducer触发强制更新
+      const [
+        ,
+        forceComponentUpdateDispatch
+      ] = useReducer(storeStateUpdatesReducer, 0);
+
+
+      // 新建一个subscription实例
+      const subscription = new Subscription(store, parentSub);
+
+      // state回调抽出来成为一个方法
+      const checkForUpdates = () => {
+        const newChildProps = childPropsSelector(store, wrapperProps);
+        // 如果参数变了，记录新的值到lastChildProps上
+        // 并且强制更新当前组件
+        if(!shallowEqual(newChildProps, lastChildProps.current)) {
+          lastChildProps.current = newChildProps;
+
+          // 需要一个API来强制更新当前组件
+          forceComponentUpdateDispatch();
+
+          // 然后通知子级更新
+          subscription.notifyNestedSubs();
+        }
+      };
+
+
+      // 使用subscription注册回调
+      subscription.onStateChange = checkForUpdates;
+      subscription.trySubscribe();
+
+      // 修改传给子级的context
+      // 将subscription替换为自己的
+      const overriddenContextValue = {
+        ...contextValue,
+        subscription
+      }
 
       // 渲染WrappedComponent
       return <WrappedComponent {...actualChildProps}></WrappedComponent>
