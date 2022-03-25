abbbf8319f2a265fbe12b0e2f547ddf873bc4b6d
Hugh 2098 <huanghuiqiang86@gmail.com>
2022-03-25 16:44:18 +0800

    feat: 接收额外参数 withExtraArgument

--- React/react-Thunk/src/redux-thunk/index.js
+++ React/react-Thunk/src/redux-thunk/index.js
@@ -1,20 +1,15 @@
 
-function thunk(store) {
-  return function (next) {
-    return function(action) {
-      // 从store中解构出dispatch, getState
-      const { dispatch, getstate } = store;
-
-      // 如果action是函数，将它拿出来运行，参数就是dispatch和getState
-      if (typeof action === 'function') {
-        return action(dispatch, getstate);
-      }
-
-      // 否则按照普通action处理
-      let result = next(action);
-      return result;
+function createThunkMiddleware(extraArgument) {
+  return ({ dispatch, getState }) => (next) => (action) => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState, extraArgument);
     }
-  }
+
+    return next(action);
+  };
 }
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
 
 export default thunk;
